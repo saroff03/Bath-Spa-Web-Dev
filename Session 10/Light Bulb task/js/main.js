d69c36d42f7893bbbe61b4ae609320e35fede378
saroff03 <106846859+saroff03@users.noreply.github.com>
2022-10-24 11:17:36 +0100

    add light bulb task

---
+++ Session 10/Light Bulb task/js/main.js
@@ -0,0 +1,7 @@
+function switchOn() {
+  document.getElementById("lightbulb").src = "images/pic_bulbon.gif"
+};
+
+function switchOff() {
+  document.getElementById("lightbulb").src = "images/pic_bulboff.gif"
+};
