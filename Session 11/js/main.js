360e16b959beeb2fef2053b1bd4e9ce94129e470
saroff03 <106846859+saroff03@users.noreply.github.com>
2022-10-25 12:52:59 +0100

    add shopping cart task

---
+++ Session 11/js/main.js
@@ -0,0 +1,73 @@
+let shoppingCart = [];
+document.getElementById("shoppingCart").innerHTML = shoppingCart;
+
+function addMilk() {
+  shoppingCart.push("Milk");
+  document.getElementById("shoppingCart").innerHTML = shoppingCart;
+  alert("Milk was added to the cart!");
+};
+
+function addBread() {
+  shoppingCart.push("Bread");
+  document.getElementById("shoppingCart").innerHTML = shoppingCart;
+  alert("Bread was added to the cart!");
+}
+
+function addEggs() {
+  shoppingCart.push("Eggs");
+  document.getElementById("shoppingCart").innerHTML = shoppingCart;
+  alert("Eggs were added to the cart!");
+}
+
+function addApples() {
+  shoppingCart.push("Apples");
+  document.getElementById("shoppingCart").innerHTML = shoppingCart;
+  alert("Apples were added to the cart!");
+}
+
+function addJuice() {
+  shoppingCart.push("Juice");
+  document.getElementById("shoppingCart").innerHTML = shoppingCart;
+  alert("Juice was added to the cart!");
+}
+
+function checkForMilk() {
+  if(shoppingCart.includes("Milk")){
+    alert("Milk is already in the cart!")
+  }
+  else {
+    addMilk();
+  }
+}
+function checkForBread() {
+  if(shoppingCart.includes("Bread")){
+    alert("Bread is already in the cart!")
+  }
+  else {
+    addBread();
+  }
+}
+function checkForEggs() {
+  if(shoppingCart.includes("Eggs")){
+    alert("Eggs are already in the cart!")
+  }
+  else {
+    addEggs();
+  }
+}
+function checkForApples() {
+  if(shoppingCart.includes("Apples")){
+    alert("Apples are already in the cart!")
+  }
+  else {
+    addApples();
+  }
+}
+function checkForJuice() {
+  if(shoppingCart.includes("Juice")){
+    alert("Juice is already in the cart!")
+  }
+  else {
+    addJuice();
+  }
+}
